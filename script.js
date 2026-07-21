1b4e62c1e406947026aba210ebe5b5537a347b95
tpham0322 <truongpham1173@gmail.com>
2026-07-21 09:37:20 -0400

    Completed task 2

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function formatFullName(firstName, lastName) {
         return "Invalid name input.";
     }
 
+    // Capitalize the first letter of each name and make the rest lowercase
     firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1).toLowerCase();
     lastName = lastName.charAt(0).toUpperCase() + lastName.slice(1).toLowerCase();
 
@@ -28,3 +29,32 @@ while (!lastName) {
 
 alert(formatFullName(firstName, lastName));
 
+
+// ==========================
+// Task 2
+// ==========================
+
+function calculateTotalCost(price, quantity, taxRate) {
+    if (isNaN(price) || isNaN(quantity) || isNaN(taxRate)) {
+        return "Invalid input.";
+    }
+
+    return (price * quantity) * (1 + taxRate);
+}
+
+let price = Number(prompt("Enter the price:"));
+while (isNaN(price)) {
+    price = Number(prompt("Invalid input. Enter the price:"));
+}
+
+let quantity = Number(prompt("Enter the quantity:"));
+while (isNaN(quantity)) {
+    quantity = Number(prompt("Invalid input. Enter the quantity:"));
+}
+
+let taxRate = Number(prompt("Enter the tax rate (Example: 0.07):"));
+while (isNaN(taxRate)) {
+    taxRate = Number(prompt("Invalid input. Enter the tax rate:"));
+}
+
+alert("Total Cost: $" + calculateTotalCost(price, quantity, taxRate).toFixed(2));
